8ef1c90bbf5e2da03f97c6e0741acdffec8a9dd0
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-04-09 15:30:41 -0500

    Final

--- src/FilmStack/urls.py
+++ src/FilmStack/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_peliculas, name="lista_peliculas"),
+    path('', views.lista_peliculas, name='lista_peliculas'),
     path('FilmStack/<int:pelicula_id>/', views.detalle_pelicula, name='detalle_pelicula'),
-    path('FilmStack/maratones', views.lista_maratones, name="lista_maratones")
+    path('FilmStack/maratones/', views.lista_maratones, name='lista_maratones'),
+    path('FilmStack/maraton/create', views.crear_maraton, name='crear_maraton'),
+    path('FilmStack/maraton/<int:maraton_id>/agregar-pelicula/', views.agregar_pelicula_maraton, name='agregar_pelicula_maraton'),
+    path('FilmStack/maraton/<int:maraton_id>/show/', views.detalle_maraton, name='detalle_maraton'),
+    path('FilmStack/maraton/<int:maraton_id>/pelicula/<int:pelicula_id>/delete/', views.quitar_pelicula_maraton, name='quitar_pelicula_maraton'),
+    path('FilmStack/maraton/<int:maraton_id>/edit/', views.actualizar_maraton, name='editar_maraton'),
+    path('FilmStack/maraton/<int:maraton_id>/delete/', views.eliminar_maraton, name='eliminar_maraton'),
 ]
--- src/FilmStack/views.py
+++ src/FilmStack/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Pelicula, Maraton
-from .forms import MaratonForm
+from .forms import MaratonForm, AgregarPeliculaForm
 
 def lista_peliculas(request):
     """Vista para ver la lista de películas con opción de búsqueda"""
@@ -11,6 +12,7 @@ def lista_peliculas(request):
         peliculas = Pelicula.objects.all()
 
     return render(request, 'FilmStack/lista_peliculas.html', {'peliculas': peliculas, 'query': query})
+
 
 def detalle_pelicula(request, pelicula_id):
     """Vista para mostrar el detalle de una películas"""
@@ -23,13 +25,81 @@ def lista_maratones(request):
     return render(request, 'FilmStack/lista_maratones.html', {'maratones': maratones})
 
 def crear_maraton(request):
-    """Vista para crear un nuevo maraton"""
+    """Vista para crear un nuevo examen"""
     if request.method == 'POST':
         form = MaratonForm(request.POST)
         if form.is_valid():
             maraton = form.save()
+            messages.success(request, 'Maratón creado correctamente.')
             return redirect('agregar_pelicula_maraton', maraton_id=maraton.id)
     else:
         form = MaratonForm()
     
     return render(request, 'FilmStack/form_maraton.html', {'form': form})
+
+def detalle_maraton(request, maraton_id):
+    """Vista para mostrar el detalle de una maratón, incluyendo sus películas."""
+    maraton = get_object_or_404(Maraton, id=maraton_id)
+    peliculas = maraton.peliculas.all()
+    return render(request, 'FilmStack/detalle_maraton.html', {
+        'maraton': maraton,
+        'peliculas': peliculas,
+    })
+
+def actualizar_maraton(request, maraton_id):
+    maraton = get_object_or_404(Maraton, id=maraton_id)
+
+    if request.method == 'POST':
+        form = MaratonForm(request.POST, instance=maraton)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Maratón actualizado correctamente.')
+            return redirect('detalle_maraton', maraton_id=maraton.id)
+    else:
+        form = MaratonForm(instance=maraton)
+
+    return render(request, 'FilmStack/form_maraton.html', {'form': form, 'maraton': maraton})
+
+
+def eliminar_maraton(request, maraton_id):
+    maraton = get_object_or_404(Maraton, id=maraton_id)
+
+    if request.method == 'POST':
+        maraton.delete()
+        messages.success(request, 'El maratón fue eliminado correctamente.')
+        return redirect('lista_maratones')
+
+    return redirect('detalle_maraton', maraton_id=maraton.id)
+
+def agregar_pelicula_maraton(request, maraton_id):
+    maraton = get_object_or_404(Maraton, id=maraton_id)
+
+    if request.method == 'POST':
+        form = AgregarPeliculaForm(request.POST)
+        if form.is_valid():
+            pelicula = form.cleaned_data['pelicula']
+            maraton.peliculas.add(pelicula)
+            messages.success(request, f'Película "{pelicula.titulo}" agregada a la maratón.')
+        
+        if 'add_another' in request.POST:
+            return redirect('agregar_pelicula_maraton', maraton_id=maraton_id)
+        elif 'finish' in request.POST:
+            return redirect('detalle_maraton', maraton_id=maraton_id)
+    else:
+        form = AgregarPeliculaForm()
+
+    return render(request, 'FilmStack/agregar_pelicula_maraton.html', {
+        'form': form,
+        'maraton': maraton,
+        'peliculas': maraton.peliculas.all()
+    })
+
+def quitar_pelicula_maraton(request, maraton_id, pelicula_id):
+    maraton = get_object_or_404(Maraton, id=maraton_id)
+    pelicula = get_object_or_404(Pelicula, id=pelicula_id)
+
+    if request.method == 'POST':
+        maraton.peliculas.remove(pelicula)
+        messages.success(request, f'La película "{pelicula.titulo}" fue eliminada del maratón.')
+    
+    return redirect('detalle_maraton', maraton_id=maraton_id)
