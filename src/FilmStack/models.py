71927e9836ba69e0a672f9bc1a8a6ca8c3225e80
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-04-09 14:20:38 -0500

    Models

--- src/FilmStack/models.py
+++ src/FilmStack/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField(blank=True)
+    poster_url = models.URLField(blank=True)
+    fecha_lanzamiento = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
+    
+class Maraton(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    peliculas = models.ManyToManyField(Pelicula, blank=True)
+
+    def __str__(self):
+        return self.nombre
+        
+    def cantidad_peliculas(self):
+        return self.peliculas.count()
